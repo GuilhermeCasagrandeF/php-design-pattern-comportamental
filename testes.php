8246ed83f1adb8a5bdef3b85e98a593385bff063
GuilhermeCasagrandeF <guilherme.casagrande@soeltech.com.br>
2023-09-05 08:54:17 -0300

    Calculadora de descontos

--- testes.php
+++ testes.php
@@ -1,14 +1,22 @@
 <?php
 
+use Alura\DesignPattern\CalculadoraDeDescontos;
 use Alura\DesignPattern\CalculadordeImpostos;
 use Alura\DesignPattern\Impostos\Icms;
 use Alura\DesignPattern\Orcamento;
 
 require_once 'vendor/autoload.php';
 
-$calculadora = new CalculadordeImpostos();
+//$calculadora = new CalculadordeImpostos();
+//$orcamento = new Orcamento();
+//
+//$orcamento->valor = 100;
+//
+//echo $calculadora->calcula($orcamento, new Icms());
+
+$calculadoraDescontos = new CalculadoraDeDescontos();
 $orcamento = new Orcamento();
+$orcamento->valor = 200;
+$orcamento->quantidadeItens = 7;
 
-$orcamento->valor = 100;
-
-echo $calculadora->calcula($orcamento, new Icms());
+echo $calculadoraDescontos->calculaDescontos($orcamento);
