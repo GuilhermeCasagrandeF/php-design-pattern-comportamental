5dea5be70dcece9faddc365076428ce1f7dc8d51
GuilhermeCasagrandeF <guilherme.casagrande@soeltech.com.br>
2023-09-05 08:53:57 -0300

    Novo ambiente

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,11 +23,30 @@ class ComposerAutoloaderInitdcceca4f011fa7d11c78338d40c0fcd5
         }
 
         spl_autoload_register(array('ComposerAutoloaderInitdcceca4f011fa7d11c78338d40c0fcd5', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitdcceca4f011fa7d11c78338d40c0fcd5', 'loadClassLoader'));
 
-        require __DIR__ . '/autoload_static.php';
-        call_user_func(\Composer\Autoload\ComposerStaticInitdcceca4f011fa7d11c78338d40c0fcd5::getInitializer($loader));
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
+        if ($useStaticLoader) {
+            require __DIR__ . '/autoload_static.php';
+
+            call_user_func(\Composer\Autoload\ComposerStaticInitdcceca4f011fa7d11c78338d40c0fcd5::getInitializer($loader));
+        } else {
+            $map = require __DIR__ . '/autoload_namespaces.php';
+            foreach ($map as $namespace => $path) {
+                $loader->set($namespace, $path);
+            }
+
+            $map = require __DIR__ . '/autoload_psr4.php';
+            foreach ($map as $namespace => $path) {
+                $loader->setPsr4($namespace, $path);
+            }
+
+            $classMap = require __DIR__ . '/autoload_classmap.php';
+            if ($classMap) {
+                $loader->addClassMap($classMap);
+            }
+        }
 
         $loader->register(true);
 
