4de060717a140fbebb976c8dfd28d3978bca41fe
GuilhermeCasagrandeF <guilherme.casagrande@soeltech.com.br>
2023-10-03 13:04:42 -0300

    Padrão projeto command

--- src/GerarPedido.php
+++ src/GerarPedido.php
@@ -2,33 +2,39 @@
 
 namespace Alura\DesignPattern;
 
-use DateTime;
-
 class GerarPedido
 {
     private float $valorOrcamento;
     private int $numeroItens;
     private string $nomeCliente;
 
-    public function __construct(float $valorOrcamento, int $numeroItens, string $nomeCliente)
-    {
+    public function __construct(float $valorOrcamento, int $numeroItens, string $nomeCliente ) {
         $this->valorOrcamento = $valorOrcamento;
         $this->numeroItens = $numeroItens;
         $this->nomeCliente = $nomeCliente;
     }
 
-    public function execute()
+    /**
+     * @return float
+     */
+    public function getValorOrcamento(): float
     {
-        $orcamento = new Orcamento();
-        $orcamento->quantidadeItens = $this->numeroItens;
-        $orcamento->valor = $this->valorOrcamento;
+        return $this->valorOrcamento;
+    }
 
-        $pedido = new Pedido();
-        $pedido->dataFinalizacao = new DateTime();
-        $pedido->nomeCliente = $this->nomeCliente;
-        $pedido->orcamento = $orcamento;
+    /**
+     * @return int
+     */
+    public function getNumeroItens(): int
+    {
+        return $this->numeroItens;
+    }
 
-        echo "Cria pedido no BD ". PHP_EOL;
-        echo "Envia email para o cliente ". PHP_EOL;
+    /**
+     * @return string
+     */
+    public function getNomeCliente(): string
+    {
+        return $this->nomeCliente;
     }
 }
